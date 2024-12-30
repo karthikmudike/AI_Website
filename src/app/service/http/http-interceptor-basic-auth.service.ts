43b1e6dde30a4f3187968b5c0261a041924e15fe
Karthik Mudike <103259823+karthikmudike@users.noreply.github.com>
2024-12-30 00:16:38 +0000

    u13

--- src/app/service/http/http-interceptor-basic-auth.service.ts
+++ src/app/service/http/http-interceptor-basic-auth.service.ts
@@ -13,11 +13,10 @@ export class HttpInterceptorBasicAuthService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler) {
 
     let modifiedRequest = request;
-    let api = "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";
     if (request.url.includes('openai.com')) {
       modifiedRequest = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${api}`
+          Authorization: `Bearer ${this.api()}`
         },
       });
     } else {
@@ -34,6 +33,10 @@ export class HttpInterceptorBasicAuthService implements HttpInterceptor {
     return next.handle(modifiedRequest);
   }
 
+  private api() {
+    let api = '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'; 
+    return api;
+   }
 
 
 }
