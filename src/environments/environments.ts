df912f6ebc1b91efb6089db99f2ef911b0a872ef
Karthik Mudike <103259823+karthikmudike@users.noreply.github.com>
2024-12-30 00:57:45 +0000

    New

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiKey: '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',
+    apiKey: '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',
     apiUrl: 'https://api.openai.com/v1/chat/completions'
   };
   
