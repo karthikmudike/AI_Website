d2be6d7f063c5fc06195c95ac4e276b0ab789c56
Karthik Mudike <103259823+karthikmudike@users.noreply.github.com>
2024-12-30 00:44:25 +0000

    Api

--- src/app/service/http/http-interceptor-basic-auth.service.ts
+++ src/app/service/http/http-interceptor-basic-auth.service.ts
@@ -2,6 +2,8 @@
 import { Injectable } from '@angular/core';
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { BasicAuthenticationService } from '../basicAuthentication.service';
+import { environment } from '../../../environments/environments';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +18,7 @@ export class HttpInterceptorBasicAuthService implements HttpInterceptor {
     if (request.url.includes('openai.com')) {
       modifiedRequest = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${this.api()}`
+          Authorization: `Bearer ${environment.apiKey}`
         },
       });
     } else {
@@ -32,11 +34,5 @@ export class HttpInterceptorBasicAuthService implements HttpInterceptor {
 
     return next.handle(modifiedRequest);
   }
-
-  private api() {
-    let api = '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'; 
-    return api;
-   }
-
 
 }
---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiKey: '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',
+    apiUrl: 'https://api.openai.com/v1/chat/completions'
+  };
+  
